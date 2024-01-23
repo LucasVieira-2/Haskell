35044c0616d96dfca59540fd76868e2d06e1d90b
Lucas Vieira <118214393+LucasVieira-2@users.noreply.github.com>
2024-01-23 18:59:15 +0000

    Update ficha1.hs

--- ficha1.hs
+++ ficha1.hs
@@ -31,4 +31,85 @@ nRaizes x y z | delta1 < 0 = 0
 			  | delta1 > 0 = 2
 			  where delta1 = (y^2)-4*x*z
 
-raizes 
+raizes :: Float -> Float -> Float -> [Float]
+raizes x y z | delta2 < 0 = [] 
+             | delta2 == 0 = [r]
+             | delta2 > 0 = [r1, r2]
+             where delta2 = (y^2) - 4*x*z
+                   r = (-y) / (2*x)
+                   r1 = ((-y) + sqrt delta2) / (2*x)
+                   r2 = ((-y) - sqrt delta2) / (2*x)
+
+
+--3
+type Hora = (Int, Int)
+
+horaValida :: Hora -> Bool
+horaValida (h, m) = if h < 24 && m < 60 then True else False
+
+horaDps :: Hora -> Hora -> Bool
+horaDps (h1, m1) (h2, m2) | h1 > h2 = True
+			  | h1 < h2 = False
+			  | h1 == h2 && m1 > m2 = True
+     			  |otherwise = False
+
+converteHoras :: Hora -> Int
+converteHoras (h, m) = h*60 + m
+
+converteMinutos :: Int -> Hora
+converteMinutos m = (div m 60, mod m 60)
+
+difHoras :: Hora -> Hora -> Int
+difHoras h1 h2 = abs(converteHoras(h1) - converteHoras(h2))
+
+adcmin :: Int -> Hora -> Hora 
+adcmin x h = converteMinutos(converteHoras(h) + x) 
+
+
+--5
+data Semaforo = Verde | Amarelo | Vermelho deriving (Show,Eq)
+--vermelho verde amarelo
+next :: Semaforo -> Semaforo
+next x | x == Amarelo = Vermelho
+next x | x == Verde = Amarelo
+next x | x == Vermelho = Verde
+
+stop :: Semaforo -> Bool
+stop x = if x == Vermelho then True else False
+
+safe :: Semaforo -> Semaforo -> Bool
+safe x y | x == Verde && y == Verde = False
+         | x == Verde && y == Amarelo = False
+         | x == Verde && y == Vermelho = True
+         | x == Amarelo && y == Verde = False
+         | x == Amarelo && y == Amarelo = False
+         | x == Amarelo && y == Vermelho = True
+         | x == Vermelho && y == Verde = True
+         | x == Vermelho && y == Amarelo = True
+         | x == Vermelho && y == Vermelho = True
+
+--6
+data Ponto = Cartesiano Double Double | Polar Double Double
+            deriving (Show,Eq)
+
+posx :: Ponto -> Double
+posx (Cartesiano x y) = x 
+posx (Polar d a) = d*(cos a)
+
+posy :: Ponto -> Double
+posy (Cartesiano x y) = y
+posy (Polar d a) = d*(sin a)
+
+raio :: Ponto -> Double
+raio (Cartesiano x y) = sqrt (x^2 + y^2)
+raio (Polar d a) = d 
+
+angulo :: Ponto -> Double
+angulo (Cartesiano 0 0) = 0
+angulo (Cartesiano 0 y1) | y1 > 0 = pi/2
+                         | y1 < 0 = -pi/2
+angulo (Cartesiano x1 y1) | x1 > 0 = atan (y1/x1)
+                          | x1 < 0 = atan (y1/x1) + pi
+angulo (Polar d a) = a
+
+--7
